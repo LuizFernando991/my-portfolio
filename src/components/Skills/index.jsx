d6a93e4258ecd09e519974e3b12f70fea9387362
unknown <lfernando.r991@gmail.com>
2024-06-13 22:09:10 -0300

    update skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,53 +1,26 @@
 import {
-  SiHtml5,
-  SiCss3,
   SiNextdotjs,
   SiTypescript,
   SiSocketdotio,
   SiTailwindcss,
-  SiPython,
   SiMongodb,
-  SiMysql,
   SiReact,
   SiExpress,
   SiPrisma,
   SiNestjs,
-  SiStyledcomponents,
   SiPostgresql,
   SiGraphql,
-  SiApollographql,
-  SiAxios,
   SiAmazonaws,
   SiDocker,
-  SiGit,
-  SiMongoose
+  SiGit
 } from 'react-icons/si'
 import { FaNode } from 'react-icons/fa'
+import { FaGolang } from 'react-icons/fa6'
 import * as Styled from './styles'
 
 export default function Skills() {
   return (
     <Styled.Container>
-      <Styled.Li>
-        <SiHtml5 />
-        <Styled.Label>HTML</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiCss3 />
-        <Styled.Label>CSS</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiReact />
-        <Styled.Label>React.js</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiReact />
-        <Styled.Label>React Native</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiNextdotjs />
-        <Styled.Label>Next.js</Styled.Label>
-      </Styled.Li>
       <Styled.Li>
         <SiTypescript />
         <Styled.Label>TypeScript</Styled.Label>
@@ -57,40 +30,36 @@ export default function Skills() {
         <Styled.Label>Node.js</Styled.Label>
       </Styled.Li>
       <Styled.Li>
+        <FaGolang />
+        <Styled.Label>Golang</Styled.Label>
+      </Styled.Li>
+      <Styled.Li>
         <SiNestjs />
         <Styled.Label>Nest.js</Styled.Label>
+      </Styled.Li>
+      <Styled.Li>
+        <SiNextdotjs />
+        <Styled.Label>Next.js</Styled.Label>
+      </Styled.Li>
+      <Styled.Li>
+        <SiReact />
+        <Styled.Label>React.js</Styled.Label>
       </Styled.Li>
       <Styled.Li>
         <SiExpress />
         <Styled.Label>Express</Styled.Label>
       </Styled.Li>
       <Styled.Li>
-        <SiAxios />
-        <Styled.Label>Axios</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
         <SiGraphql />
         <Styled.Label>GraphQL</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiApollographql />
-        <Styled.Label>Apollo Client</Styled.Label>
       </Styled.Li>
       <Styled.Li>
         <SiSocketdotio />
         <Styled.Label>Socket.io</Styled.Label>
       </Styled.Li>
       <Styled.Li>
-        <SiPython />
-        <Styled.Label>Python</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
         <SiMongodb />
         <Styled.Label>MongoDB</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiMysql />
-        <Styled.Label>MySQL</Styled.Label>
       </Styled.Li>
       <Styled.Li>
         <SiPostgresql />
@@ -99,14 +68,6 @@ export default function Skills() {
       <Styled.Li>
         <SiPrisma />
         <Styled.Label>Prisma</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiMongoose />
-        <Styled.Label>Mongoose</Styled.Label>
-      </Styled.Li>
-      <Styled.Li>
-        <SiStyledcomponents />
-        <Styled.Label>Styled Components</Styled.Label>
       </Styled.Li>
       <Styled.Li>
         <SiTailwindcss />
